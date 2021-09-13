e8d92e914e12dad9d8226004c3d2a5aeac3b3d8e
Smikey <smikeywork@gmail.com>
2021-09-14 00:10:44 +0545

    :sparkles: Created User Banner Slider Component

---
+++ src/components/UserBannerSlider.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react'
+
+class UserBannerSlider extends Component {
+    render() {
+        return (
+            <div>
+                  {/* banner */}
+        <div className="banner-text">
+          <div className="callbacks_container">
+            <ul className="rslides" id="slider3">
+              <li className="banner">
+                <div className="container">
+                  <h3 className="agile_btxt">
+                    e<span>f</span>ashion
+                    <span>S</span>tore
+                  </h3>
+                  <h4 className="w3_bbot">shop exclusive clothing</h4>
+                  <div className="slider-info mt-sm-5">
+                    <h4 className="bn_right">
+                      <span>w</span>omen's
+                      <span>f</span>ashion</h4>
+                    <div className="bnr_clip position-relative">
+                      <h4>get up to
+                        <span className="px-2">45% </span>
+                      </h4>
+                      <div className="d-inline-flex flex-column banner-pos position-absolute text-center">
+                        <div className="bg-flex-item">
+                          <span>O</span>
+                        </div>
+                        <div className="bg-flex-item">
+                          <span>F</span>
+                        </div>
+                        <div className="bg-flex-item">
+                          <span>F
+                          </span>
+                        </div>
+                      </div>
+                      <p className="text-uppercase py-2">on special sale</p>
+                      <a className="btn btn-primary mt-3 text-capitalize" href="shop.html" role="button">shop now</a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className="banner banner2">
+                <div className="container">
+                  <h3 className="agile_btxt">
+                    e<span>f</span>ashion
+                    <span>S</span>tore
+                  </h3>
+                  <h4 className="w3_bbot">shop exclusive clothing</h4>
+                  <div className="slider-info mt-sm-5">
+                    <h4 className="bn_right">
+                      <span>m</span>en's
+                      <span>f</span>ashion</h4>
+                    <div className="bnr_clip position-relative">
+                      <h4>get up to
+                        <span className="px-2">35% </span>
+                      </h4>
+                      <div className="d-inline-flex flex-column banner-pos position-absolute text-center">
+                        <div className="bg-flex-item">
+                          <span>O</span>
+                        </div>
+                        <div className="bg-flex-item">
+                          <span>F</span>
+                        </div>
+                        <div className="bg-flex-item">
+                          <span>F
+                          </span>
+                        </div>
+                      </div>
+                      <p className="text-uppercase py-2">on special sale</p>
+                      <a className="btn btn-primary mt-3 text-capitalize" href="shop.html" role="button">shop now</a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className="banner banner3">
+                <div className="container">
+                  <h3 className="agile_btxt">
+                    e<span>f</span>ashion
+                    <span>S</span>tore
+                  </h3>
+                  <h4 className="w3_bbot">shop exclusive clothing</h4>
+                  <div className="slider-info mt-sm-5">
+                    <h4 className="bn_right">
+                      <span>k</span>id's
+                      <span>f</span>ashion</h4>
+                    <div className="bnr_clip position-relative">
+                      <h4>get up to
+                        <span className="px-2">45% </span>
+                      </h4>
+                      <div className="d-inline-flex flex-column banner-pos position-absolute text-center">
+                        <div className="bg-flex-item">
+                          <span>O</span>
+                        </div>
+                        <div className="bg-flex-item">
+                          <span>F</span>
+                        </div>
+                        <div className="bg-flex-item">
+                          <span>F
+                          </span>
+                        </div>
+                      </div>
+                      <p className="text-uppercase py-2">on special sale</p>
+                      <a className="btn btn-primary mt-3 text-capitalize" href="shop.html" role="button">shop now</a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* //banner */}
+            </div>
+        )
+    }
+}
+
+export default UserBannerSlider
