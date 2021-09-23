3d5ce372083e19975ac2b0888f69313849c7aaf2
Smikey <smikeywork@gmail.com>
2021-09-23 19:39:12 +0545

    :hammer: Updating product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,74 +1,74 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 class Product extends Component {
 
- state = {
-        product: {},
-        myproducts: [],
-        id: this.props.match.params.id,
-        productImageUrlList: []
-          
-    }
-    componentDidMount() {
-        axios.get("http://localhost:90/product/get")
-            .then((res) => {
-                this.setState({
-                    myproducts: res.data.data
-                })
-            })    
-             
-        axios.get("http://localhost:90/product/get/" + this.state.id)
-            .then(res => {
-              console.log(res)
-                this.setState({
-                    product: res.data.singleProductData,
-                    productImageUrlList :res.data.singleProductData.productImageUrlList
-                })
-                
+  state = {
+    product: {},
+    myproducts: [],
+    id: this.props.match.params.id,
+    productImageUrlList: []
 
-            })
-            .catch()
-    }
-
-        addToCart = (productId) => {
-        const token = localStorage.getItem("token")
-        console.log(token)
-        console.log(productId)
-        console.log("http://localhost:90/cart/insert/" + productId)
-        axios.post("http://localhost:90/cart/insert/" + productId, {productId},{
-            headers: {
-                'authorization': "Bearer " + token,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-
+  }
+  componentDidMount() {
+    axios.get("http://localhost:90/product/get")
+      .then((res) => {
+        this.setState({
+          myproducts: res.data.data
         })
-        alert("added")
+      })
 
-    }
+    axios.get("http://localhost:90/product/get/" + this.state.id)
+      .then(res => {
+        console.log(res)
+        this.setState({
+          product: res.data.singleProductData,
+          productImageUrlList: res.data.singleProductData.productImageUrlList
+        })
+
+
+      })
+      .catch()
+  }
+
+  addToCart = (productId) => {
+    const token = localStorage.getItem("token")
+    console.log(token)
+    console.log(productId)
+    console.log("http://localhost:90/cart/insert/" + productId)
+    axios.post("http://localhost:90/cart/insert/" + productId, { productId }, {
+      headers: {
+        'authorization': "Bearer " + token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+
+    })
+    alert("added")
+
+  }
 
   render() {
-     return (
+    return (
       <div>
         {/* inner banner */}
         <div className="ibanner_w3 pt-sm-5 pt-3">
           <h4 className="head_agileinfo text-center text-capitalize text-center pt-5">
-            <span>f</span>ashion
-            <span>h</span>ub</h4>
+            e<span>f</span>ashion
+            <span>S</span>ore</h4>
         </div>
         {/* //inner banner */}
         {/* breadcrumbs */}
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
             <li className="breadcrumb-item">
-              <a href="index.html">Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li className="breadcrumb-item">
-              <a href="women.html">Women's Clothing</a>
+              <Link to="/product">Product</Link>
             </li>
-            <li className="breadcrumb-item active" aria-current="page">Single Product</li>
+            <li className="breadcrumb-item active" aria-current="page">{this.state.product._id}</li>
           </ol>
         </nav>
         {/* //breadcrumbs */}
@@ -79,18 +79,17 @@ class Product extends Component {
               <div className="col-lg-4 single-right-left">
                 <div className="grid images_3_of_2">
                   <div className="flexslider1">
-                   
                     <ul className="slides">
                       {
-                      this.state.productImageUrlList.map((productImg)=> {
-                        return (
-                         <li data-thumb={productImg}>
-                        <div className="thumb-image">
-                          <img src={productImg} data-imagezoom="true" alt=" " className="img-fluid" /> </div>
-                        </li>
-                        )
+                        this.state.productImageUrlList.map((productImg) => {
+                          return (
+                            <li data-thumb={productImg}>
+                              <div className="thumb-image">
+                                <img src={productImg} data-imagezoom="true" alt=" " className="img-fluid" /> </div>
+                            </li>
+                          )
                         })
-                    }
+                      }
                     </ul>
                     <div className="clearfix" />
                   </div>
@@ -101,31 +100,44 @@ class Product extends Component {
                 <h3>{this.state.product.productName}</h3>
                 <div className="caption">
                   <ul className="rating-single">
+                    {/* Rating Stars of product */}
+
+                    {/* {
+                      this.state.product.productRating.map((rate) => {
+                        return (
+                          <li>
+                            <Link to="#">
+                              <span className="fa fa-star yellow-star" aria-hidden="true" />
+                            </Link>
+                          </li>
+                        )
+                      })
+                    } */}
+
                     <li>
-                      <a href="#">
+                      <Link to="#">
                         <span className="fa fa-star yellow-star" aria-hidden="true" />
-                      </a>
+                      </Link>
                     </li>
+
                     <li>
-                      <a href="#">
+                      <Link to="#">
                         <span className="fa fa-star yellow-star" aria-hidden="true" />
-                      </a>
+                      </Link>
                     </li>
+
                     <li>
-                      <a href="#">
+                      <Link to="#">
                         <span className="fa fa-star yellow-star" aria-hidden="true" />
-                      </a>
+                      </Link>
                     </li>
+
                     <li>
-                      <a href="#">
+                      <Link to="#">
                         <span className="fa fa-star yellow-star" aria-hidden="true" />
-                      </a>
+                      </Link>
                     </li>
-                    <li>
-                      <a href="#">
-                        <span className="fa fa-star yellow-star" aria-hidden="true" />
-                      </a>
-                    </li>
+
                   </ul>
                   <div className="clearfix"> </div>
                   <h6>Rs. {this.state.product.productPrice}</h6>
@@ -151,10 +163,10 @@ class Product extends Component {
                     <h5 className="sp_title mb-3">services</h5>
                     <ul className="single_serv">
                       <li>
-                        <a href="#">30 Day Return Policy</a>
+                        <Link to="#">30 Day Return Policy</Link>
                       </li>
                       <li className="mt-2">
-                        <a href="#">Cash on Delivery available</a>
+                        <Link to="#">Cash on Delivery available</Link>
                       </li>
                     </ul>
                   </div>
@@ -166,16 +178,44 @@ class Product extends Component {
                     <input type="submit" defaultValue="Check" />
                   </form>
                 </div>
+
+                {/* Need this cart cart
                 <div className="occasion-cart">
                   <div className="chr single-item single_page_b">
-                    
-                      <button type="submit" className="hub-cart phub-cart btn" onClick={()=>this.addToCart(this.state.id)}>
+                    <form action="#" method="post">
+                      <input type="hidden" name="cmd" defaultValue="_cart" />
+                      <input type="hidden" name="add" defaultValue={1} />
+                      <input type="hidden" name="hub_item" defaultValue=" Formal Men's Blazer" />
+                      <input type="hidden" name="amount" defaultValue="15.99" />
+                      <button type="submit" className="hub-cart phub-cart btn">
                         <i className="fa fa-cart-plus" aria-hidden="true" />
                       </button>
-                                    
+                      <a href="#" data-toggle="modal" data-target="#myModal1" />
+                    </form>
                   </div>
                 </div>
+                 */}
+
+                {/* working cart */}
+                <div className="occasion-cart">
+                  <div className="chr single-item single_page_b">
+                    <button type="submit" className="hub-cart phub-cart btn" onClick={() => this.addToCart(this.state.id)}>
+                      <i className="fa fa-cart-plus" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+
+                {/* for wishlist
+                <div className="occasion-cart">
+                  <div className="chr single-item single_page_b">
+                    <button type="submit" className="hub-cart phub-cart btn" onClick={() => this.addToWishlist(this.state.id)}>
+                      <i className="fa fa-heart text-danger" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div> */}
+
               </div>
+
             </div>
           </div>
         </div>
@@ -187,43 +227,54 @@ class Product extends Component {
               </h4>
 
               {/* card group 2 */}
-                  <div className="card-group my-5">
-                    {
-         this.state.myproducts.slice(0,4).map((product) => {
-            return (
-              // {/* card */}
-                  <div className="col-lg-3 col-sm-6 p-0">
-                    <div className="card product-men p-3">
-                      <div className="men-thumb-item">
-                        <img src="images/pm1.jpg" alt="img" className="card-img-top" />
-                        <div className="men-cart-pro">
-                          <div className="inner-men-cart-pro">
-                            <NavLink to={"/product/" + product._id} className="link-product-add-cart">Quick View</NavLink>
+              <div className="card-group my-5">
+                {
+                  this.state.myproducts.slice(6, 10).map((product) => {
+                    return (
+                      // {/* card */}
+                      <div className="col-lg-3 col-sm-6 p-0">
+                        <div className="card product-men p-3">
+                          <div className="men-thumb-item">
+                            <img src={product.productImageUrlList[0]} alt="img" className="card-img-top" />
+                            <div className="men-cart-pro">
+                              <div className="inner-men-cart-pro">
+                                <NavLink to={"/product/" + product._id} className="link-product-add-cart">Quick View</NavLink>
+                              </div>
+                            </div>
+                          </div>
+                          {/* card body */}
+                          <div className="card-body  py-3 px-2">
+                            <h5 className="card-title text-capitalize">{product.productName}</h5>
+                            <div className="card-text d-flex justify-content-between">
+                              <p className="text-dark font-weight-bold">Rs. {product.productPrice}</p>
+                              <p className="line-through">Rs. {product.productCuttedPrice}</p>
+                            </div>
+                          </div>
+                          {/* card footer */}
+                          <div className="card-footer d-flex justify-content-between">
+                            {/* for cart */}
+                            <button type="submit" className="hub-cart phub-cart btn" onClick={() => this.addToCart(product._id)} >
+                              <i className="fa fa-cart-plus" aria-hidden="true" />
+                            </button>
+
+                            {/* for wishlist  */}
+                            <div className="occasion-cart">
+                              <div className="chr single-item single_page_b">
+                                <button type="submit" className="hub-cart phub-cart btn" onClick={() => this.addToWishlist(product._id)}>
+                                  <i className="fa fa-heart text-danger" aria-hidden="true" />
+                                </button>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
-                      {/* card body */}
-                      <div className="card-body  py-3 px-2">
-                        <h5 className="card-title text-capitalize">{product.productName}</h5>
-                        <div className="card-text d-flex justify-content-between">
-                          <p className="text-dark font-weight-bold">Rs. {product.productPrice}</p>
-                          <p className="line-through">Rs. {product.productCuttedPrice}</p>
-                        </div>
-                      </div>
-                      {/* card footer */}
-                      <div className="card-footer d-flex justify-content-end">
-                          <button type="submit" className="hub-cart phub-cart btn" onClick={()=>this.addToCart(product._id)} >
-                            <i className="fa fa-cart-plus" aria-hidden="true" />
-                          </button>                      
-                      </div>
-                    </div>
-                  </div>
-                  // {/* //card */}
-            )
-          })
-        }
-                  </div>
-                  {/* //card group */}    
+                      // {/* //card */}
+                    )
+                  })
+                }
+              </div>
+              {/* //card group */}
+
               {/*//new_arrivals*/}
             </div>
           </div>
