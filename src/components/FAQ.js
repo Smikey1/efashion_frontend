187be20f65d4460a2dd320e277d6cd596fc7c846
Smikey <smikeywork@gmail.com>
2021-09-23 19:28:00 +0545

    :sparkles: Created FAQ section

---
+++ src/components/FAQ.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react'
+
+class FAQ extends Component {
+  render() {
+    return (
+      <div>
+        {/* inner banner */}
+        <div className="ibanner_w3 pt-sm-5 pt-3">
+          <h4 className="head_agileinfo text-center text-capitalize text-center pt-5">
+            e<span>f</span>ashion
+            <span>S</span>ore</h4>
+        </div>
+        {/* //inner banner */}
+        {/* FAQ-help-page */}
+        <div className="help about faq-sec">
+          <div className="container">
+            <h3 className="w3-head">How can we help you</h3>
+            <div className="faq-w3agile">
+              <h5>Top 10 Frequently asked questions(FAQ)</h5>
+              <ul className="faq">
+                <li className="item1">
+                  <a href="#" title="click here">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor vehicula ipsum nec ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
+                        ut laoreet dolore. At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+                        praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi
+                        sint occaecati cupiditate non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item2">
+                  <a href="#" title="click here">The standard Lorem Ipsum passage Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet
+                    ultrices ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p> Tincidunt ut laoreet dolore At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing
+                        elit, sed diam nonummy nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh
+                        euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                        deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate
+                        non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item3">
+                  <a href="#" title="click here">Consectetuer adipiscing elit Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet
+                    ultrices?
+                  </a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>Dincidunt ut laoreet dolore At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing
+                        elit, sed diam nonummy nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh
+                        euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                        deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate
+                        non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item4">
+                  <a href="#" title="click here">Sed diam nonummy nibh euismod Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet
+                    ultrices?
+                  </a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                        nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et
+                        iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item5">
+                  <a href="#" title="click here">Euismod tincidunt laoreet Etiam faucibus viverra libero vel efficitur ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                        nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et
+                        iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item6">
+                  <a href="#" title="click here">Voluptas sit aspernatur aut Ut semper nisl ut laoreet ultrices ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                        nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et
+                        iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item7">
+                  <a href="#" title="click here">Donec ut quam ligula feugiat Ut semper nisl ut laoreet ultrices ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                        nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et
+                        iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item8">
+                  <a href="#" title="click here">The standard Lorem Ipsum Ut semper nisl ut laoreet ultrices passage ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>Lorem ipsum dolor sit amet At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing
+                        elit, sed diam nonummy nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh
+                        euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                        deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate
+                        non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item9">
+                  <a href="#" title="click here">Consectetuer adipiscing Ut semper nisl ut laoreet ultrices elit ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>Lorem ipsum dolor sit amet At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing
+                        elit, sed diam nonummy nibh euismod consectetuer adipiscing elit, sed diam nonummy nibh
+                        euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                        deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate
+                        non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="item10">
+                  <a href="#" title="click here">Sed diam nonummy Ut semper nisl ut laoreet ultrices nibh euismod ?</a>
+                  <ul>
+                    <li className="subitem1">
+                      <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus qui blanditiis
+                        praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi
+                        sint occaecati cupiditate non provident.</p>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FAQ
