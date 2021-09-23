b1fed4c12fec412b40527c37476abd5264c8966c
Smikey <smikeywork@gmail.com>
2021-09-23 19:46:59 +0545

    :hammer: importing require file to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import {Route,Switch} from 'react-router-dom'
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home.js';
+import About from "./pages/About";
 import AdminDashboard from './pages/AdminDashboard.js';
+import Blog from "./pages/Blog";
+import BlogCollection from "./pages/BlogCollection";
+import Checkout from "./pages/Checkout";
+import Contact from "./pages/Contact";
+import FAQ from "./pages/FAQ";
+import Girl from "./pages/Girl";
+import GirlCollection from "./pages/GirlCollection";
+import Home from './pages/Home.js';
+import Login from './pages/Login';
+import Payment from "./pages/Payment";
+import Product from "./pages/Product";
+import ProductCollection from "./pages/ProductCollection";
+import Profile from "./pages/Profile";
+import Register from './pages/Register';
+import Wishlist from "./pages/Wishlist";
 
 function App() {
   return (
@@ -17,6 +29,20 @@ function App() {
        <Route exact path="/login" component={Login}/>
        <Route exact path="/register" component={Register}/>
        <Route path="*" render={()=>"404 Not found!! "}/>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/blogCollection" component={BlogCollection} />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/wishlist" component={Wishlist} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/faq" component={FAQ} />
+      <Route path="/payment" component={Payment} />
+      <Route path="/girlCollection" component={GirlCollection} />
+      <Route path="/productCollection" component={ProductCollection} />
+      <Route path="/girl" component={Girl} />
+      <Route path="/product/:id" component={Product} />
      </Switch>
       
   );
